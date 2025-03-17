86ff27c7a27e7ac2417c60f7f3d619630eee35af
unknown <sandip.rai@westcliff.edu>
2025-03-17 07:38:32 +0545

    Ingredient List and Recipe added.

--- week2/chefmate.js
+++ week2/chefmate.js
@@ -1,3 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const checkboxes = document.querySelectorAll(".ingredient-list input");
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener("change", function() {
+            if (this.checked) {
+                this.parentElement.style.textDecoration = "line-through";
+            } else {
+                this.parentElement.style.textDecoration = "none";
+            }
+        });
+    });
+});
+
 function toggleMenu() {
     document.getElementById("navLinks").classList.toggle("show");
+}
+
+function printRecipe() {
+    window.print();
 }
